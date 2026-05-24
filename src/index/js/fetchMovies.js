8fd9d67b7970f750259a9d8c476f2673e99f74a4
James <jwyork2008@gmail.com>
2026-05-24 15:44:46 -0500

    Made random take into account the possibility of a response less than 500 pages

--- src/index/js/fetchMovies.js
+++ src/index/js/fetchMovies.js
@@ -16,8 +16,6 @@ let getSelectedGenres = () => {
 }
 
 let handleRandom = async () => {
-    const randomPage = Math.floor(Math.random() * 500) + 1
-
     const selectedGenres = getSelectedGenres().join(',')
     let fromYear = document.getElementById('fromYear').value
     let toYear = document.getElementById('toYear').value
@@ -38,14 +36,25 @@ let handleRandom = async () => {
         params.append('primary_release_date.lte', `${toYear}-12-31`)
     }
 
-    let url = `https://api.themoviedb.org/3/discover/movie?${params}&page=${randomPage}`
-    const res = await fetch(url)
+    let url = `https://api.themoviedb.org/3/discover/movie?${params}`
+    let res = await fetch(url)
 
     if (!res.ok) {
         throw new Error(`HTTP ${res.status}`)
     }
 
-    const data = await res.json()
+    let data = await res.json()
+
+    let randomPage
+    data.total_pages <= 500 
+        ? randomPage = Math.floor(Math.random() * data.total_pages) + 1
+        : randomPage = Math.floor(Math.random() * 500) + 1
+
+    url = `https://api.themoviedb.org/3/discover/movie?${params}&page=${randomPage}`
+
+    res = await fetch(url)
+    data = await res.json()
+    
     const randomMovie = data.results[Math.floor(Math.random() * data.results.length)]
 
     return randomMovie
