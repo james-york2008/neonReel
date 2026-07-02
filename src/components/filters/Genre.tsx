1413bf14f4a678447f1213156ea20d5c90c5e9c4
James <jwyork2008@gmail.com>
2026-07-02 18:22:17 -0500

    Implemented enter key genre selecting

--- src/components/filters/Genre.tsx
+++ src/components/filters/Genre.tsx
@@ -3,12 +3,27 @@ import type { Genre } from "../../types/genre"
 type Props = {
   genre: Genre
   genreChange: (genre: number) => void
+  selectedGenres: number[]
 }
 
-export default function Genres({ genre, genreChange }: Props) {
+export default function Genres({ genre, genreChange, selectedGenres }: Props) {
   return(
     <label htmlFor={genre.genreLabel} className="genreContainer">
-      <input type="checkbox" id={genre.genreLabel} value={genre.id} onChange={() => genreChange(genre.id)} />
+      <input 
+        type="checkbox" 
+        id={genre.genreLabel} 
+        value={genre.id} 
+        onChange={() => genreChange(genre.id)} 
+        onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
+          if (event.key === 'Enter') {
+            genreChange(genre.id)
+          }
+        }}
+        checked={selectedGenres.includes(genre.id) 
+          ? true
+          : false
+        } />
+
       <p className="genre">{genre.genreName}</p>
     </label>
   )
