1a0301c774135b41873dafb3bd16e5b56e83cb30
James <jwyork2008@gmail.com>
2026-07-03 18:08:49 -0500

    Implemented React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,16 @@
-import { useEffect, useState, useCallback } from "react";
-import type { Movie } from "./types/movie";
-import { fetchRecommendedMovies, searchMovies, handleFilters, handleRandom } from "./components/fetchMovies";
-
-import Hero from "./components/hero/Hero";
-import Filters from "./components/filters/Filters"
-import Movies from "./components/movies/Movies"
-import Random from "./components/random/Random"
-import Footer from "./components/footer/Footer"
-
-import heroImage from "./assets/heroImage.png"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/mainPage/MainPage";
+import ErrorPage from "./pages/errorPage/ErrorPage";
 
 export default function App() {  
-  const [movies, setMovies] = useState<Movie[]>([])
-  const [selectedGenres, setSelectedGenres] = useState<number[]>([])
-  const [fromYear, setFromYear] = useState<number>()
-  const [toYear, setToYear] = useState<number>()
-  const [debounceTimeout, setDebounceTimeout] = useState<number | null>(null)
-
-  useEffect(() => {
-    async function loadMovies() {
-      const res = await fetchRecommendedMovies()
-      setMovies(res)
-    }
-
-    loadMovies()
-  }, [])
-
-  async function handleSearch(query: string) {
-    const res = await searchMovies(query)
-    setMovies(res)
-  }
-
-  async function genreChange (genre: number) {    
-    let updatedGenres
-    if (selectedGenres.includes(genre)) {
-      updatedGenres = selectedGenres.filter((item) => item !== genre)
-    } else {
-      updatedGenres = [...selectedGenres, genre]
-    }
-    setSelectedGenres(updatedGenres)
-
-    const res = await handleFilters(updatedGenres, fromYear, toYear)
-    setMovies(res)
-  }
-
-  const yearFilter = useCallback(
-    async (fromYearNumber: number | undefined, toYearNumber: number | undefined) => {
-      if (debounceTimeout) {
-        clearTimeout(debounceTimeout)
-      }
-      
-      if (!fromYearNumber) fromYearNumber = fromYear
-      if (!toYearNumber) toYearNumber = toYear
-    
-      setFromYear(fromYearNumber)
-      setToYear(toYearNumber)
-
-      
-      const newTimeout = setTimeout(async () => {
-        const res = await handleFilters(selectedGenres, fromYearNumber, toYearNumber)
-        setMovies(res)    
-      }, 300)
-
-      setDebounceTimeout(newTimeout)
-    }, [debounceTimeout]
-  )  
-
-  async function randomMovie() {
-    const res = await handleRandom(selectedGenres, fromYear, toYear)
-    setMovies(res)
-  }
-
   return (
-    <>
-      <main>
-        <div id="backgroundImageWrapper" style={{
-        backgroundImage: `url(${heroImage})`}}>
-          <Hero onSearch={handleSearch} />
-          <Filters genreChange={genreChange} yearFilter={yearFilter} selectedGenres={selectedGenres} />
-        </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/neonReel" element={<MainPage />} />
+
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
       
-        <Movies movies={movies} />
-        <Random handleRandom={randomMovie} />
-      </main>
-      <Footer />
-    </>
+    </BrowserRouter>
   )
 }
