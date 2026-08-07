622a5b41eba3d2ca96817195bfffaa0468e62844
James <jwyork2008@gmail.com>
2026-08-07 13:52:18 -0500

    Adding taglines and vote counts to the movie pages

--- src/pages/moviePage/components/movie/Movie.tsx
+++ src/pages/moviePage/components/movie/Movie.tsx
@@ -10,34 +10,40 @@ type Props = {
 export default function MovieElement ({ movie }: Props) {
 
   if (movie) {
+    console.log(movie)
     return (
       <>
         <div className={styles.moviePageMainContent}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`${movie.title} backdrop image`} className={styles.movieImage} />
-          <div>
-            <h1 className={styles.movieTitle}>{movie.title}</h1>
-          
-            <div className={`${styles.moviePageRow} ${styles.greyText}`}>
-              <span className="screenReaderText">Released in</span>
-              <p>{movie.release_date.split("-")[0]}</p>
-              
-              <span className="screenReaderText">Runtime</span>
-              <p>{movie.runtime} min</p>
+          <div className={styles.mainMovieWrapper}>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={`${movie.title} backdrop image`} className={styles.movieImage} />
+            <div>
+              <h1 className={styles.movieTitle}>{movie.title}</h1>
+            
+              <div className={`${styles.moviePageRow} ${styles.greyText}`}>
+                <span className="screenReaderText">Released in</span>
+                <p>{movie.release_date.split("-")[0]}</p>
+                
+                <span className="screenReaderText">Runtime</span>
+                <p>{movie.runtime} min</p>
 
-              <span className="screenReaderText">Status:</span>
-              <p className={movie.status !== "Released" ? styles.movieStatusNotReleased : ""}>{movie.status}</p>
-            </div>
-  
-            <div className={`${styles.moviePageRow} ${styles.greyText}`}>
-              <div className={styles.starRatingContainer}>
-                <span className="screenReaderText">Star rating</span>
-                <p className={styles.starRating}>{movie.vote_average.toFixed(1)} / 10</p>
-                <img src={star} alt="" aria-hidden="true" className={styles.starIcon} />
+                <span className="screenReaderText">Status:</span>
+                <p className={movie.status !== "Released" ? styles.movieStatusNotReleased : ""}>{movie.status}</p>
               </div>
+    
+              <div className={`${styles.moviePageRow} ${styles.greyText}`}>
+                <div className={styles.starRatingContainer}>
+                  <span className="screenReaderText">Star rating</span>
+                  <p className={styles.starRating}>{movie.vote_average.toFixed(1)} / 10</p>
+                  <img src={star} alt="" aria-hidden="true" className={styles.starIcon} />
+                  <p>({movie.vote_count.toLocaleString()})</p>
+                  <span className="screenReaderText">Votes</span>
+                </div>
+              </div>
+    
+              <p className={styles.movieOverview}>{movie.overview}</p>
             </div>
-  
-            <p className={styles.movieOverview}>{movie.overview}</p>
-          </div>
+          </div>  
+          <p className={styles.tagline}>{movie.tagline}</p>
         </div>
       </>
     ) 
